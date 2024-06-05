35b6eb44245392f84ea62e80434658d4651f4e26
Razmik Mnatsakanyan <razmik.mnatsakanyann@gmail.com>
2024-06-06 03:15:47 +0400

    Version 2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-
 console.log('aaaaaarrrggghhhhh');
+
+
+function reverseWords(text){
+  const textArray = text.split();
+  let reverseArray
+  for(let i = 0; i < textArray.length; i++){
+    reverseArray[i] += textArray[textArray.length - (i+1)];
+  }
+  return reverseArray.join(' ');
+}
+eshinDgha();
+console.log(reverseWords('eshin dgha'));
